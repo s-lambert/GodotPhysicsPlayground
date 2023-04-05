5e2b196ee62ae296c2edc358d61a9d5771927c51
Scott Lambert <s-lambert@live.com.au>
2023-04-05 20:00:37 +0800

    Add an enemy to chase the player

---
+++ enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+const SPEED = 4.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var player = $%Player
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	else: 
+		var player_position = player.global_position
+		var enemy_position = self.global_position
+		var difference = player_position - enemy_position
+		if difference.length() < 5.0:
+			var direction = difference.normalized()
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
